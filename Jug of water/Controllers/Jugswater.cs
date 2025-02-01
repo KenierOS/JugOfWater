b6c91a9af28b5baada845acda7d32ec629473a4a
Kenier <141670712+KenierOS@users.noreply.github.com>
2025-02-01 16:28:35 -0400

    Add files via upload

--- Jug of water/Controllers/Jugswater.cs
+++ Jug of water/Controllers/Jugswater.cs
@@ -13,12 +13,13 @@ namespace Jug_of_water.Controllers
         [HttpPost("resultado")]
         public IActionResult Resultado([FromBody] JarrasCapacidad capacidad)
         {
-            //If: 
+            //if: to not allow the entry of non-integer values
             if (capacidad.X <= 0 || capacidad.Y <= 0 || capacidad.Z <= 0) 
             {
                 return BadRequest(new { error = "Not allowed values only positive numbers and integers" });
             }
 
+            //Var: to determine if the exercise has a solution with the data entered
             var solucion = SolucionJarras(capacidad.X, capacidad.Y, capacidad.Z);
 
             if (solucion == null)
@@ -30,18 +31,20 @@ namespace Jug_of_water.Controllers
         }
 
 
-        
 
+        //list: sequence of steps that the program executes to solve the exercise
         public List<Secuencia> SolucionJarras(int X, int Y, int Z)
         {
+            //If: which executes the function with the greatest common divisor of the data
             if (Z > Math.Max(X, Y) || Z % MaximoComunDivisor(X, Y) != 0)
 
             {
                 return null;
             }
-
+            //Function that determines which jar to start with to achieve the most optimal result
             bool inicio = EleccionDeJarra(X, Y, Z);
 
+            //steps to follow with the while loop until you have the result
             var paso = new List<Secuencia>();
 
             int xActual = 0; int yActual = 0;
@@ -134,7 +137,7 @@ namespace Jug_of_water.Controllers
             return paso;
         }
 
-
+        //greatest common divisor function
         public int MaximoComunDivisor( int a, int b)
         {
             while (b != 0)
@@ -146,6 +149,7 @@ namespace Jug_of_water.Controllers
             return a;
         }
 
+        //Function to choose which jar to start 
         public bool EleccionDeJarra(int X, int Y, int Z)
         {
             if (Z <= X)
@@ -162,7 +166,7 @@ namespace Jug_of_water.Controllers
             }
         }
 
-
+        //class of data requested from the user
         public class JarrasCapacidad
         {
             
@@ -172,6 +176,7 @@ namespace Jug_of_water.Controllers
 
         }
 
+        //data that is displayed each time a step is taken in the exercise
         public class Secuencia
         {
             public int Steps { get; set; }
--- Jug of water/Program.cs
+++ Jug of water/Program.cs
@@ -1,32 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
-
-// Configuración para mostrar mensajes de error detallados
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(e => e.ErrorMessage).ToArray());
-
-        var result = new BadRequestObjectResult(errors);
-        return result;
-    };
-});
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
 
 app.UseHttpsRedirection();
 
